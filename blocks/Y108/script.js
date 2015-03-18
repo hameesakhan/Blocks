0baefe4646bbbae40c8cb0f689ceee47039e13da
Mustafijur Rahman Nebir <nebir1993@gmail.com>
2015-03-18 17:41:00 +0600

    Block Y103 is finished

---
+++ blocks/Y108/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $("#client-speech").owlCarousel({
+        autoPlay: 3000,
+        navigation : false, // Show next and prev buttons
+        slideSpeed : 700,
+        paginationSpeed : 1000,
+        singleItem:true
+    });
+});

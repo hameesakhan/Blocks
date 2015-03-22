5db29701870292eefb04db9be7395ac8023abb2f
Mustafijur Rahman Nebir <nebir1993@gmail.com>
2015-03-22 16:18:19 +0600

    blockP007 finished

---
+++ blocks/P007/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $("#slider-right").owlCarousel({
+        autoPlay: 3000,
+        navigation : false, // Show next and prev buttons
+        slideSpeed : 700,
+        paginationSpeed : 1000,
+        singleItem:true
+    });
+});
